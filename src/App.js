edb4853e98866705906883a743e15696ad0d6fa5
Jyjystudio <jyhad.benyoussef@gmail.com>
2021-06-03 17:30:19 +0200

    second way to pass a function in react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Formation React</h1>
-//     </div>
-//   );
-// }
-
-const App = () => {
+function App() {
   return (
     <div className="App">
       <h1>Formation React</h1>
     </div>
   );
 }
+
 export default App;
