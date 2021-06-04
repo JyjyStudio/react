842ef9bd362486888c3e917910e88cb94c32a5ad
Jyjystudio <jyhad.benyoussef@gmail.com>
2021-06-04 23:30:04 +0200

    third way to pass a function in react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import React from 'react';
 import './App.css';
 
 // function App() {
+  // return (
+  //   <div className="App">
+  //     <h1>Formation React</h1>
+  //   </div>
+  // );
+// }
+
+// const App = () => {
 //   return (
 //     <div className="App">
 //       <h1>Formation React</h1>
 //     </div>
 //   );
 // }
-
-const App = () => {
-  return (
-    <div className="App">
-      <h1>Formation React</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Formation React</h1>
+      </div>
+    );
+  }
 }
 export default App;
